92a2619984f7846f85e3d05e6c454bf52c75996d
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-10-09 11:28:25 +0200

    buttons__item renamed to buttons__button

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,13 +4,13 @@ const Buttons = ({ tasks, hideDoneTasks }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button className="buttons__item"
+                <button className="buttons__button"
                     disabled={tasks.every(({ done }) => done)}
                 >
                     Ukończ wszystkie
                 </button>
 
-                <button className="buttons__item">
+                <button className="buttons__button">
                     {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
                 </button>
             </>
