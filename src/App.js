28b1a3011bcfc397538c64ea386552e7b5192530
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-10-09 15:32:24 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -40,6 +40,22 @@ function App() {
     })));
   };
 
+  const addNewTask = (newTaskContent) => {
+    setTasks(tasks => {
+      if (newTaskContent !== "") {
+        return [
+          ...tasks,
+          {
+            content: newTaskContent,
+            done: false,
+            id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+          },
+        ];
+      };
+      return tasks;
+    })
+  };
+
   return (
     <Container>
 
@@ -47,7 +63,7 @@ function App() {
 
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section
