6f3cf26dd19a3579b2fa78d879c0a6a668cc8c56
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-20 10:52:41 +0100

    search feature with dynamic URL update added

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,9 +1,32 @@
+import { useLocation, useHistory } from "react-router-dom";
+import Input from "../../Input";
+import { Wrapper } from "./styled";
+
 const Search = () => {
+    const location = useLocation();
+    const history = useHistory();
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get("szukaj");
+
+    const onInputChange = ({ target }) => {
+        if (target.value.trim() === "") {
+            searchParams.delete("szukaj");
+        } else {
+            searchParams.set("szukaj", target.value)
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
+
     return (
-        <>
-            nana
-        </>
-    )
+        <Wrapper>
+            <Input
+                placeholder="Filtruj zadania"
+                value={query || ""}
+                onChange={onInputChange}
+            />
+        </Wrapper>
+    );
 };
 
 export default Search;
