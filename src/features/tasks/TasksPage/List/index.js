367e7d80da8fff6152a6d36997e1ba778354c3b5
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-20 11:02:25 +0100

    search query parameter name extracted to a separate file

--- src/features/tasks/TasksPage/List/index.js
+++ src/features/tasks/TasksPage/List/index.js
@@ -2,11 +2,12 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { TasksList, ListItem, ListContent, ToggleDoneButton, RemoveButton } from "./styled";
 import { selectTasksByQuery, selectTasksState, toggleTaskDone, removeTask } from "../../tasksSlice";
+import searchQueryParamName from "../searchQueryParamName";
 
 const List = () => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
-    const query = searchParams.get("szukaj");
+    const query = searchParams.get(searchQueryParamName);
 
     const tasks = useSelector(state => selectTasksByQuery(state, query));
     const hideDone = useSelector(selectTasksState);
--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,18 +1,19 @@
 import { useLocation, useHistory } from "react-router-dom";
 import Input from "../../Input";
 import { Wrapper } from "./styled";
+import searchQueryParamName from "../searchQueryParamName";
 
 const Search = () => {
     const location = useLocation();
     const history = useHistory();
     const searchParams = new URLSearchParams(location.search);
-    const query = searchParams.get("szukaj");
+    const query = searchParams.get(searchQueryParamName);
 
     const onInputChange = ({ target }) => {
         if (target.value.trim() === "") {
-            searchParams.delete("szukaj");
+            searchParams.delete(searchQueryParamName);
         } else {
-            searchParams.set("szukaj", target.value)
+            searchParams.set(searchQueryParamName, target.value)
         }
 
         history.push(`${location.pathname}?${searchParams.toString()}`);
