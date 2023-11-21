052d8d2710b1695731c1a961c3083ada3fcac8c4
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-21 08:31:00 +0100

    replaced <p> with <Chapter>

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,6 +1,7 @@
 import Section from "../../common/Section/index.js";
 import Header from "../../common/Header/index.js";
 import { StyledContainer } from "../../common/styled.js";
+import { Chapter } from "./styled.js";
 
 function Author() {
     return (
@@ -10,12 +11,12 @@ function Author() {
                 title="Patrycja Gomza"
                 body={
                     <>
-                        <p>
+                        <Chapter>
                             Ukończyłam studia na kierunku budownictwo lądowe i wodne na <strong>Politechnice Wrocławskiej</strong> i przez ostatnie 8 lat pracowałam jako inżynier w biurze projektowym, zajmując się projektowaniem konstrukcji budynków.
-                        </p>
-                        <p>
+                        </Chapter>
+                        <Chapter>
                             Jestem osobą ambitną, która w wyzwaniach widzi możliwości rozwoju, a innych ludzi traktuje ze szczerością i szacunkiem. Ponadto <strong>cechuje mnie wysokie poczucie odpowiedzialności oraz konsekwencja</strong>, co znajduje odzwierciedlenie w moim <i><u>hobby</u></i>. Interesuję się szkoleniem psów, gdzie obowiązkowość i dobra organizacja są ważnym elementem nie tylko codziennego treningu, ale również życia. 👩🏼💼🐕‍🦺
-                        </p>
+                        </Chapter>
                     </>
                 }
             />
---
+++ src/features/author/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Chapter = styled.p`
+    text-align: justify;
+    line-height: 1.15;
+`;
